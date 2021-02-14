464a9d79c17596c65cd85c1176170b3fcbb3e10e
ancientmelody <ndavidkelly@gmail.com>
2021-02-14 12:19:46 -0500

    added menu dropdown

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,24 @@
-// Steps:
-    // Style in CSS (responsive topnav or slide-in menu)
 // JS behaviour:
-    // When user clicks hamburger, nav items appear
-    // When user clicks again, nav items disappear
     // When user clicks nav item, menu disappears
 // Responsive:
     // Hamburger icon only appears after a certain breakpoint
 
 
-// Target hamburger icon and add event listener
+// Target hamburger icon, dropdown and menu links
 const hamburger = document.querySelector(".hamburger");
+const dropdown = document.querySelector('.dropdown');
+const menuItems = document.querySelectorAll('.menu-items');
+
+// Add event listener to hamburger icon + menu items
 hamburger.addEventListener('click', hamburgerToggle);
+menuItems.addEventListener('click', menuToggle);
 
 // Function to run when hamburger is clicked
 function hamburgerToggle() {
-    hamburger.classList.toggle('change');
+    hamburger.classList.toggle('changeham');
+    dropdown.classList.toggle('showdropdown');
+}
+
+function menuToggle() {
+    dropdown.classList.toggle('showdropdown');
 }
